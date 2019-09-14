c29fa1ef9188a579f08ad12cce4ddaaa29984ab9
Keisuke Fukuda <kfukuda@preferred.jp>
2019-09-14 19:50:32 +0900

    commit

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,6 +8,12 @@ mpiexec -genvall -n 1 pipenv run test
 rm -rf test_tmp
 mkdir -p test_tmp
 
-head -c 1024 /dev/urandom >test_tmp/random.txt
-md5sum test_tmp/*
+F=test_tmp/random.txt
+head -c 1024 /dev/urandom >$F
+SUM=$(md5sum $F | cut -f 1 -d ' ')
+
+
+mpiexec -n 2 python mpicpy/mpicpy.py $F --rank-suffix
+ls $F.1
+
 
